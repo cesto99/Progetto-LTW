65ff3a6ff2d568af691e826c5a8f6749b70e6faa
EnriWildBandana <nerofort99@gmail.com>
2021-05-07 16:18:53 +0200

    implementato validazione php

--- Sito Web Bar Sapienza/Registrazione/Registrazione.php
+++ Sito Web Bar Sapienza/Registrazione/Registrazione.php
@@ -5,19 +5,17 @@
     <body>
         <?php
             $dbconn = pg_connect("host=drinkparty.cpbsxsr2pb7t.us-east-1.rds.amazonaws.com port=5432 dbname=barsapienza
-                                    user=postgres password=ciaociao99")
+            user=postgres password=ciaociao99")
             or die('Could not connect:'.pg_last_error() );
             if (!(isset($_POST ['reg']))) {
                 header("Location: ../index.html");
-
             }
             else {
                 $username = $_POST['user'];
-                $q1= "select * from utenti where username=$1";
+                $q1= "select * from utenti where username = $1";
                 $result = pg_query_params($dbconn, $q1, array($username));
-                if ($line=pg_fetch_array($result, null, PSGQL_ASSOC)) {
-                    echo "<h1> Utente giá registrato </h1> 
-                            <a href=../index.html> Torna al login </a>";
+                if ($line=pg_fetch_array($result, null, PGSQL_ASSOC)) {
+                    echo "<h1> Utente giá registrato </h1> <a href=../index.html> Torna al login </a>";
                 }
                 else {
                     $username = $_POST ['user'];
@@ -26,7 +24,7 @@
                     $q2 = "insert into utenti values($1, $2, $3)";
                     $data = pg_query_params( $dbconn, $q2, array ($username, $password, $email));
                     if ($data) {
-                       echo "<h1> Registrazione completata.</h1>";
+                       echo "<h1> Registrazione completata.</h1>"; //Non si vede questo messaggio
                        header("Location: ../Utente_Loggato.html");
                     }
                 }  
--- Sito Web Bar Sapienza/Registrazione/Validazione.php
+++ Sito Web Bar Sapienza/Registrazione/Validazione.php
@@ -1,34 +1,36 @@
-<?php
-    session_start();
-
-    
-    
-    $con = mysqli_connect('localhost', 'root');
-
-    
-    
-    mysqli_select_db($con, 'registrazione_utente');
-
-    
-
-    $name = $_POST['user'];
-    $pass = $_POST['password'];
-
-  
-    $s = " select * from user_table where name = '$name' && password = '$pass'";
-
-    $result = mysqli_query($con, $s);
-
-    $num = mysqli_num_rows($result);
-
-    if($num == 1){
-        
-        $_SESSION['username']=$name;
-        header('location:Home.php');
-    }else{
-           
-            header('location:Login.php');
-        }
-    
-
-?>
+<html>
+    <head></head>
+    <body>
+        <?php
+            $dbconn = pg_connect("host=drinkparty.cpbsxsr2pb7t.us-east-1.rds.amazonaws.com port=5432 dbname=barsapienza
+            user=postgres password=ciaociao99")
+            or die('Could not connect: '. pg_last_error());
+            if(!(isset($_POST['acc']))){
+                header("Location: ../index.html");
+            }
+            else{
+                $username = $_POST['user'];
+                $q1="select * from utenti where username = $1";
+                $result=pg_query_params($dbconn, $q1, array($username));
+                if (!($line=pg_fetch_array($result, null, PGSQL_ASSOC))){
+                    echo "<h1> Spiacente, non sei un utente registrato </h1>
+                    <a href=signup.html>
+                    Clicca qui per registrarti
+                    </a>";  
+                }
+                else{
+                    $password = $_POST['password']; // con md5 non funzionava
+                    $q2 = "select * from utenti where username = $1 and pass = $2";
+                    $result = pg_query_params($dbconn, $q2, array ($username, $password));
+                    if (!($line = pg_fetch_array($result, null, PGSQL_ASSOC))){
+                        echo "<h1> La password è errata </h1>";    
+                    }
+                    else {
+                        echo "<h1>Accesso eseguito con successo</h1>"; //Non si vede questo messaggio
+                        header("Location: ../Utente_Loggato.html");
+                    }
+                }
+            }
+        ?>
+    </body>
+</html>

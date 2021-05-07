adef9abbc86140ed858768a1e4b6c530197b9cd8
EnriWildBandana <nerofort99@gmail.com>
2021-05-07 12:16:59 +0200

    registrazione php

--- Sito Web Bar Sapienza/Registrazione/Registrazione.php
+++ Sito Web Bar Sapienza/Registrazione/Registrazione.php
@@ -1,38 +1,37 @@
-<?php
-    session_start();
-    //$con = mysqli_connect('localhost', 'root');
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $dbconn = pg_connect("host=drinkparty.cpbsxsr2pb7t.us-east-1.rds.amazonaws.com port=5432 dbname=barsapienza
+                                    user=postgres password=ciaociao99")
+            or die('Could not connect:'.pg_last_error() );
+            if (!(isset($_POST ['reg']))) {
+                header("Location: ../index.html");
 
-    //mysqli_select_db($con, 'registrazione_utente');
-
-    $dbconn=pg_connect("host=localost port=5500 
-            dbname=registrazione_utente
-            user=root")
-            or die('ould not connect: ' . preg_last_error());
-
-    $name = $_POST['user'];
-    $pass = $_POST['password'];
-
-  
-    $s = " select * from user_table where name = '$name' && password = '$pass'";
-
-    //$result = mysqli_query($con, $s);
-    $result = mysqli_query($dbconn, $s);
-   
-    $num = mysqli_num_rows($result);
-
-    if($num == 1){
-        echo " Utente già esistente!";
-        header('location:Login.php');
-        
-     
-    }else{
-            $reg = " insert into user_table(Nome, Password) values ('$name' , '$pass')";
-            mysqli_query($con, $reg);
-            echo " Registrazione andata a buon fine!";
-            header('location:Home.php');
-            
-        }
-    
-
-?>
+            }
+            else {
+                $username = $_POST['user'];
+                $q1= "select * from utenti where username=$1";
+                $result = pg_query_params($dbconn, $q1, array($username));
+                if ($line=pg_fetch_array($result, null, PSGQL_ASSOC)) {
+                    echo "<h1> Utente giá registrato </h1> 
+                            <a href=../index.html> Torna al login </a>";
+                }
+                else {
+                    $username = $_POST ['user'];
+                    $password = $_POST ['password'];
+                    $email = $_POST ['email'];
+                    $q2 = "insert into utenti values($1, $2, $3)";
+                    $data = pg_query_params( $dbconn, $q2, array ($username, $password, $email));
+                    if ($data) {
+                       echo "<h1> Registrazione completata.</h1>";
+                       header("Location: ../Utente_Loggato.html");
+                    }
+                }  
+            }
+        ?>
+    </body>
+</html>
 

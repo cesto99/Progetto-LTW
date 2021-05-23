d5c950cd767d5c0775217ab3bd0af700f98190f2
Andrea <andrea99hp@hotmail.it>
2021-05-23 18:27:54 +0200

    Negozio

--- Sito Web Bar Sapienza/intestazione.js
+++ Sito Web Bar Sapienza/intestazione.js
@@ -1,43 +1,15 @@
 function caricaPagine(){
-    $("#intest").append(''+
-    '<div>'+
-        '<img src="./icons/Uniroma1.svg.png" alt="">'+
-    '</div>'+
-    '<div class="mobile-nav">'+
-        '<div class="nav-top">'+
-            '<ul>'+
-                '<li><a href="index.html">Home</a></li>'+
-                '<li><a href="Info.html">Info</a></li>'+
-                '<li><a href="#">Negozio</a></li>'+
-                '<li><a href="./Login.html">Registrati | Accedi </a></li>'+
-            '</ul>'+
-        '</div>'+
-        '<div class="contact flex items-center">'+
-            '<img src="./icons/phone.svg" alt="">'+
-            '<div>'+
-                '<h5>SOS - 800 81 11 92</h5>'+
-                '<h6>E-mail : helpdesk@studenti.uniroma1.it</h6>'+
-            '</div>'+
-        '</div>'+
-        '<div class="time flex items-center">'+
-           '<img src="./icons/clock.svg" alt="">'+
-           '<div>'+
-                '<h5>Orario </h5>'+
-                '<h6>Lun - Sat 8.00 - 12.00</h6>'+
-            '</div>'+
-        '</div>'+
-    '</div>');
-if (localStorage.utente==undefined){
+ if (localStorage.utente==undefined){
     $("#inf").append('' +
     '<div class="container flex justify-between items-center">'+
         '<div class="icons">'+
             '<a href="#"><img src="./icons/facebook.svg" alt=""></a>'+
-            '<a href="#"><img src="./icons/instagram.svg" alt=""></a>'+
+            '<a href="https://www.instagram.com/sapienzaroma/?hl=it"><img src="./icons/instagram.svg" alt=""></a>'+
         '</div>'+
         '<div class="auth flex items-center">'+
             '<div>'+
                 '<img src="./icons/user-icon.svg" alt="">'+
-                '<a href="Registrazione/login.html">Log in</a>'+
+                '<a href="Registrazione/login.html">Login</a>'+
             '</div>'+
             '<span class="divider">|</span>'+
             '<div>'+
@@ -65,10 +37,16 @@ else{
                     '<a href="./Registrazione/Logout.php"> Logout</a>'+
                 '</div>'+
             '</div>'+
+            '<div>'+
+                '<img src="./icons/cart.svg" style="width:25px; height:25px; margin-left:40px"></img>'+
+                '<p id="carr" style="position relative; float:right; font-size:18px"></p>'+
+                '<p style="margin-left:40px">Carrello</p>'+
+            '</div>'+
         '</div>'+
     '</div>');
 
     scriviNome();
+    document.getElementById("carr").innerText=localStorage.carrello;
 }
 
     $("#menu").append(''+
@@ -95,8 +73,7 @@ else{
         '<div class="container flex justify-center">'+
             '<a href="index.html" >Home</a>'+
             '<a href="Info.html">Info</a>'+
-            '<a href="#">Metodo di pagamento</a>'+
-            '<a href="#">Negozio</a>'+
+            '<a class="neg" href="Negozio.html">Negozio</a>'+
             '<a href="#">Contatti</a>'+
         '</div>'+
     '</div>');
@@ -112,7 +89,7 @@ else{
             '</div>'+
             '<div class="service-card magic-shadow-sm">'+
                 '<img class="icon" src="./icons/usd.svg" alt="">'+
-                '<h2>Te lo consegnamo noi!</h2>'+
+                '<h2>Te lo consegniamo noi!</h2>'+
                 "<p>Con un tempo di attesa stabilito te lo consegnamo noi a seconda dell'edificio in cui ti trovi</p>"+
             '</div>'+
         '</div>'+
@@ -129,7 +106,7 @@ else{
             '<ul>'+
                 '<li><a href="index.html">Home</a></li>'+
                 '<li><a href="Info.html">Info</a</li>'+
-                '<li><a href="#">Negozio</a</li>'+
+                '<li><a class="neg" href="Negozio.html">Negozio</a</li>'+
                 '<li><a href="Registrazione/Login.html">Registrati o Accedi</a></li>'+
             '</ul>'+
         '</div>'+
@@ -153,5 +130,17 @@ else{
             '</div>'+
         '</div>'+
     '</div>');
-
+    
+    
+        var arr=document.getElementsByClassName("neg");
+        var n=arr.length;
+        for(var i=0;i<n;i++){
+            if(localStorage.utente==undefined){
+                arr[i].href="./Registrazione/Login.html";
+            }
+            else {
+                arr[i].href="Negozio.html";
+            }
+        }
 }
+
